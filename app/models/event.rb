c8b272cf1aa707820709042813561069d9779284
RobPavl <robert.pavlovsky@gmail.com>
2015-09-01 18:44:25 +0300

    Views to Events (not fixed).

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
-   has_many :places, trough: :event_places
-   has_many :cars, trough: :event_cars
+   has_many :places, through: :event_places
+   has_many :cars, through: :event_cars
 end
